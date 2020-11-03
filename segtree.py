db31975f0df7d2652ca3ec426019f62b182d5749
fgshun <fgshun@gmail.com>
2020-11-03 21:08:53 +0900

    遅延セグメント木

--- segtree.py
+++ segtree.py
@@ -15,9 +15,10 @@ class Monoid(NamedTuple):
 
 class SegumentTree(Generic[T]):
     """セグメント木"""
-    mo: Monoid
+    monoid: Monoid
     length: int
     data: MutableSequence[T]
+    lazy: MutableSequence[T]
 
     def __init__(self, seq: Iterable[T], monoid: Monoid = Monoid(operator.add, int)) -> None:
         self.monoid = monoid
@@ -28,22 +29,44 @@ class SegumentTree(Generic[T]):
         self.length = length
         ie = monoid.ie
         self.data = [ie() for _ in range(len(data) * 4)]
+        self.lazy = [ie() for _ in range(len(data) * 4)]
         for i, v in enumerate(data):
-            self.update(i, v)
+            s = i
+            e = s + 1
+            self.update(s, e, v)
 
-    def update(self, i: int, v: T) -> None:
-        if self.monoid.is_group:
-            assert self.monoid.is_group(v)
-        index = self.length + i - 1
-        self.data[index] = v
-        while index > 0:
-            index = (index - 1) // 2
-            self.data[index] = self.monoid.bo(self.data[index * 2 + 1], self.data[index * 2 + 2])
+    def eval(self, i: int) -> None:
+        lazy = self.lazy[i]
+        ie = self.monoid.ie()
+
+        if lazy == ie:
+            return
+
+        if i < self.length - 1:
+            self.lazy[i * 2 + 1] = lazy
+            self.lazy[i * 2 + 2] = lazy
+
+        self.data[i] = lazy
+        self.lazy[i] = ie
+
+    def update(self, start: int, end: int, value: T) -> None:
+        self._update(start, end, value, 0, 0, self.length)
+
+    def _update(self, a: int, b: int, v: T, k: int, l: int, r: int) -> None:
+        self.eval(k)
+        if a <= l and r <= b:  # 完全に含む。
+            self.lazy[k] = v
+            self.eval(k)
+        elif a < r and l < b:
+            self._update(a, b, v, k * 2 + 1, l, (l + r) // 2)
+            self._update(a, b, v, k * 2 + 2, (l + r) // 2, r)
+            self.data[k] = self.monoid.bo(self.data[k * 2 + 1], self.data[k * 2 + 2])
 
     def query(self, start: int, end: int) -> T:
         return self._query(start, end, 0, 0, self.length)
 
     def _query(self, a: int, b: int, k: int, l: int, r: int) -> T:
+        self.eval(k)
         if r <= a or b <= l:  # 交差しない。単位元を返す
             return self.monoid.ie()
         if a <= l and r <= b:  # 完全に含む。
@@ -63,14 +86,21 @@ def main() -> None:
     # mo = Monoid(operator.add, int)
     # mo = Monoid(operator.mul, lambda: 1)
 
-    a = SegumentTree(range(10000), mo)
-    s, e = 11, 9374
-    # s, e = 1, 5
+    n, s, e = 100000, 11, 9374
+    # n, s, e = 16, 1, 5
+
+    a = SegumentTree(range(n), mo)
     b = a.query(s, e)
 
     from functools import reduce
     print(b, reduce(mo.bo, range(s, e)))
-    print(a.query(0, 10000), reduce(mo.bo, range(10000)))
+    print(a.query(0, n), reduce(mo.bo, range(n)))
+    a.update(3, 6, 0)
+    print(a.query(s, e))
+    print(a.query(3, 7))
+    print(a.query(6, 7))
+    # print(a.data[:32])
+    # print(a.lazy[:32])
 
 
 if __name__ == '__main__':
