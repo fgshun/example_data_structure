eb98fa137d283597c0a0eddc89dd4950844a9f5a
Ito Shunsuke <fgshun@gmail.com>
2021-08-27 19:04:09 +0900

    union by size 版を追加。連結成分の大きさがわかる

--- unionfindtree.py
+++ unionfindtree.py
@@ -24,3 +24,34 @@ class UnionFindTree:
 
     def same(self, x: int, y: int) -> bool:
         return self.find(x) == self.find(y)
+
+
+class UnionFindTreeSize:
+    def __init__(self, n: int) -> None:
+        self.par = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x: int) -> int:
+        if self.par[x] == x:
+            return x
+        else:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+
+    def unite(self, x: int, y: int) -> None:
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+        if self.size[x] < self.size[y]:
+            self.par[x] = y
+            self.size[y] += self.size[x]
+        else:
+            self.par[y] = x
+            self.size[x] += self.size[y]
+
+    def same(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+    def get_size(self, x: int) -> int:
+        return self.size[self.find(x)]
